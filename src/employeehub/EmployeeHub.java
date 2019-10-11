bcdd28bc9b1da419e99e7b646dd95b252c1b921b
loti-ibrahimi <loti.ibrahimi@outlook.com>
2019-10-11 01:55:24 +0100

    New Beginning

---
+++ src/employeehub/EmployeeHub.java
@@ -0,0 +1,12 @@
+package employeehub;
+
+/**
+ *
+ * @author loti ibrahimi
+ */
+
+public class EmployeeHub {
+    public static void main(String[] args) {
+        System.out.println("Simple CRUD App");
+    }
+}
